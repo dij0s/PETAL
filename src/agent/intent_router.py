c147658df3d61060748503fc94ba280da82fed8f
dij0s <redrouge03@gmail.com>
2025-05-21 13:46:37 +0200

    May remove all custom parsing supprt..

--- src/agent/intent_router.py
+++ src/agent/intent_router.py
@@ -6,7 +6,7 @@ from langchain_ollama import ChatOllama
 from modelling.PydanticStreamOutputParser import PydanticStreamOutputParser
 
 from typing import Optional
-from pydantic import BaseModel, ValidationError
+from pydantic import BaseModel, Field, ValidationError
 
 router_prompt = PromptTemplate.from_template("""
 You are an AI assistant helping to route user requests about energy planning in Switzerland.
@@ -15,37 +15,31 @@ Classify the user input into:
 - topic: e.g. "solar", "biomass", "heating", "wind"
 - location: if available (e.g. a municipality name)
 
-Return ONLY the following JSON like this, with no extra text, explanation, or formatting:
-
-{{
-"intent": "...",
-"topic": "...",
-"location": "..."
-}}
-
 User input: "{user_input}"
 """)
 
 class RouterOutput(BaseModel):
     """Router output used to route user queries to appropriate agents and retrieve basic context"""
 
-    intent: Optional[str]
-    topic: Optional[str]
-    location: Optional[str]
+    intent: Optional[str] = Field(description="The intent of the user, must be one of ['data_request', 'planning_request', 'policy_question']")
+    topic: Optional[str] = Field(description="The main topic of the user request, e.g. 'solar', 'biomass', 'heating', 'wind'")
+    location: Optional[str] = Field(description="The location mentioned in the user request, if available (e.g. a municipality name)")
 
-
-llm = ChatOllama(model="llama3.2:3b", temperature=0)
+llm = ChatOllama(model="llama3.2:3b", temperature=0).with_structured_output(RouterOutput)
 parser = PydanticStreamOutputParser(pydantic_object=RouterOutput, diff=True)
 
 async def router_llm_node(state):
     last_human_message = next(msg.content for msg in reversed(state["messages"]) if isinstance(msg, HumanMessage))
-    prompt = router_prompt.format(user_input=last_human_message) + parser.get_format_instructions()
+    prompt: str = router_prompt.format(user_input=last_human_message) + parser.get_format_instructions()
 
     output = await llm.ainvoke(prompt)
+    content = output.content
 
     try:
         parsed = parser.parse(output.content)
         print(parsed)
+        if parsed is None:
+            raise Exception("No parsed output from Pydantic object")
     except Exception as e:
         print(f"Error: {e}")
         parsed = RouterOutput(intent=None, topic=None, location=None)
