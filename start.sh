96d389930f0d7e0c86dd705b0a7100906528eae3
dij0s <redrouge03@gmail.com>
2025-06-18 21:50:56 +0200

    Created convenience script to properly choose environment..

---
+++ start.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# default to development
+ENVIRONMENT=${1:-dev}
+if [[ "$ENVIRONMENT" != "dev" && "$ENVIRONMENT" != "prod" ]]; then
+    echo "ERROR: Environment must be either 'dev' or 'prod'"
+    echo "Usage: ./start.sh [dev|prod] [docker-compose args]"
+    exit 1
+fi
+
+shift 1 2>/dev/null || true
+echo "Starting in $ENVIRONMENT mode..."
+
+set -a
+if [[ ! -f ".env.$ENVIRONMENT" ]]; then
+    echo "ERROR: .env.$ENVIRONMENT file does not exist."
+    exit 1
+fi
+source .env.$ENVIRONMENT
+set +a
+
+# export ENVIRONMENT
+# for interpolation in
+# compose definition
+export ENVIRONMENT
+docker compose up "$@"
