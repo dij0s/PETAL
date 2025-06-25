33a3412f902b2e82a73fb0c86972e9ae68b01970
dij0s <redrouge03@gmail.com>
2025-06-25 12:51:41 +0200

    Wrapping up..

--- backend/src/agent/intent_router.py
+++ backend/src/agent/intent_router.py
@@ -101,7 +101,6 @@ async def intent_router(state, *, config: RunnableConfig, store: BaseStore):
     human_messages: list[str] = [msg.content for msg in reversed(state.messages) if isinstance(msg, HumanMessage)] # type: ignore
     last_human_message: str = human_messages[0] if human_messages else ""
     previous_human_message: str = human_messages[1] if len(human_messages) > 1 else ""
-    print(f"The last human message is '{last_human_message}' and the previous one is '{previous_human_message}'")
     # retrieve curent router context
     # and fill it in the prompt for
     # context carry over
