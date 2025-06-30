29e40898b4a5dcc46efaae4072b0a3293557696c
dij0s <redrouge03@gmail.com>
2025-06-30 18:08:52 +0200

    Properly attaching the callbacks in the Chat wrapper..

--- backend/src/provider/ModelProvider.py
+++ backend/src/provider/ModelProvider.py
@@ -1,16 +1,49 @@
 import os
 
+from langchain_core.callbacks import AsyncCallbackHandler, BaseCallbackManager
 from langchain_ollama import ChatOllama
+
+from typing import Any, Optional
+
+class MyCustomCallback(AsyncCallbackHandler):
+    async def on_chat_model_start(self, serialized: dict[str, Any], messages, *, run_id, parent_run_id = None, tags: Optional[list[str]] = None, metadata: Optional[dict[str, Any]] = None, **kwargs: Any) -> Any:
+        return await super().on_chat_model_start(serialized, messages, run_id=run_id, parent_run_id=parent_run_id, tags=tags, metadata=metadata, **kwargs)
+
+    async def on_llm_end(self, response, *, run_id, parent_run_id = None, tags: Optional[list[str]] = None, **kwargs: Any) -> None:
+        return await super().on_llm_end(response, run_id=run_id, parent_run_id=parent_run_id, tags=tags, **kwargs)
+
+class ChatOllamaWrapper(ChatOllama):
+    """
+    The following class wraps the ChatOllama implementation.
+    """
+
+    def __init__(self, model: str, temperature: float, **kwargs):
+        handlers = kwargs.get("callbacks", []) + [MyCustomCallback()]
+        manager: BaseCallbackManager = BaseCallbackManager(handlers=handlers, inheritable_handlers=handlers)
+        super().__init__(model=model, temperature=temperature, **kwargs, callbacks=manager)
+
+    async def ainvoke(
+        self,
+        input,
+        config=None,
+        *,
+        stop=None,
+        **kwargs
+    ):
+        return await super().ainvoke(input=input, config=config, stop=stop, **kwargs)
 
 class ModelProvider():
     """
     The following class implements a factory pattern for model instance creation.
     """
 
+    def __new__(cls, *args, **kwargs):
+        raise TypeError("ModelProvider cannot be instantiated. Use class methods only.")
+
     @staticmethod
-    def from_env_variable(env_variable: str, temperature: float, defaults: str, **kwargs) -> ChatOllama:
+    def from_env_variable(env_variable: str, temperature: float, defaults: str, **kwargs) -> ChatOllamaWrapper:
         """
-        Creates a ChatOllama model instance using parameters from an environment variable.
+        Creates a ChatOllamaWrapper model instance using parameters from an environment variable.
 
         Args:s
             env_variable (str): The name of the environment variable containing the model name.
@@ -18,7 +51,7 @@ class ModelProvider():
             defaults (str): Default model name if the environment variable is not set.
 
         Returns:
-            ChatOllama: An instance of the ChatOllama model.
+            ChatOllamaWrapper: A wrapper of the ChatOllama model instance.
         """
         OLLAMA_HOST = os.getenv("OLLAMA_HOST", "")
         MODEL = os.getenv(env_variable, defaults)
@@ -27,6 +60,6 @@ class ModelProvider():
             # default to local
             # ollama installation
             # if host is not set
-            return ChatOllama(model=MODEL, temperature=temperature, **kwargs)
+            return ChatOllamaWrapper(model=MODEL, temperature=temperature, **kwargs)
         else:
-            return ChatOllama(model=MODEL, temperature=temperature, base_url=OLLAMA_HOST, **kwargs)
+            return ChatOllamaWrapper(model=MODEL, temperature=temperature, base_url=OLLAMA_HOST, **kwargs)
